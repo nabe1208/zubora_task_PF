37f68ce3ba59a4796e2beeae44e62bfe225a3f8f
nabe1208 <air.love60@.gmail.com>
2022-06-25 16:14:32 +0000

    log-in

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def unsubscribe
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def others
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  # homes
+  root to: 'homes#top'
+  get "/support" => "homes#support", as: 'support'
+
   devise_for :users
+  # user
+   resources :users, only: [:edit,:index,:update,:destroy]
+   get "user/mypage" => "users#mypage", as: 'mypage'
+   get 'user/unsubscribe' => "users#unsubscribe", as: 'unsubscribe'
+   get 'user/:id' => "users#others", as: 'others'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
